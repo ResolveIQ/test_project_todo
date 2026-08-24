244cf918d14f0e17ea557484d1a1e36f9c2d8fe2
ram9096 <ashaysudharsan2000@gmail.com>
2026-08-24 07:52:56 +0530

    Fix: Update API Key

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -2,18 +2,21 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import dotenv from "dotenv"
+dotenv.config()
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCeW1zfTVOpiTasaEquNaqufo6UTuNYSOA",
+  apiKey: process.env.API_KEY,
   authDomain: "context-based-todo.firebaseapp.com",
   projectId: "context-based-todo",
   storageBucket: "context-based-todo.firebasestorage.app",
   messagingSenderId: "54341318440",
-  appId: "1:54341318440:web:27ae6fc72d6481afdbc21d",
+  appId: process.env.APP_ID,
   measurementId: "G-NS7H36R5WD"
 };
 
